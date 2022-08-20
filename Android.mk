52ea19c9548e9430cfc9e8edb9d547835807be88
Hakimi0804 <hakimifirdaus944@gmail.com>
2022-08-20 22:49:46 +0800

    RMX2001: Rebrand to RM6785
    Signed-off-by: Hakimi0804 <hakimifirdaus944@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),RMX2001)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifndef RMX2001_BUILD
+    RMX2001_BUILD := false
+endif
+
+ifeq ($(TARGET_DEVICE),RM6785)
+    include $(call all-subdir-makefiles,$(LOCAL_PATH))
+else ifeq ($(TARGET_DEVICE),RMX2001)
+    ifeq ($(RMX2001_BUILD),false)
+        $(warning "Target device is RMX2001 but RMX2001_BUILD variable is not set")
+        $(warning "Recovery size will be 98MB")
+    endif
+    include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
