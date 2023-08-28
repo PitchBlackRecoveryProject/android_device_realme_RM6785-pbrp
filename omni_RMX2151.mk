947abeb5aff6c0340db47ba2a742a6211b892290
Ansh <singhansh64321@gmail.com>
2023-08-28 03:31:29 +0000

    RMX2151: Adapt for PBRP
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- omni_RMX2151.mk
+++ omni_RMX2151.mk
@@ -10,14 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common PBRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from RMX2151 device
 $(call inherit-product, device/realme/RMX2151/device.mk)
 
 PRODUCT_DEVICE := RMX2151
-PRODUCT_NAME := twrp_RMX2151
+PRODUCT_NAME := omni_RMX2151
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme 7
 PRODUCT_MANUFACTURER := realme
